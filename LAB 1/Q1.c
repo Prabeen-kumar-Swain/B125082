9966d39cd95fc006d8e6d5e622fa3b9b384c535b
Prabeen Kumar Swain <b125082@iiit-bh.ac.in>
2026-07-31 19:36:42 +0530

    Add array statistics function
    Implement array statistics to find largest, smallest, and average values.

--- LAB 1/Q1.c
+++ LAB 1/Q1.c
@@ -1,1 +1,34 @@
+//1. Array Statistics
+#include <stdio.h>
 
+void arrayStats(int arr[], int n) {
+    int largest = arr[0], smallest = arr[0];
+    float sum = 0, avg;
+    
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+        if (arr[i] < smallest)
+            smallest = arr[i];
+        sum += arr[i];
+    }
+    avg = sum/n;
+  
+    printf("The largest number in the array is : %d\n", largest);
+    printf("The smallest number in the array is : %d\n", smallest);
+    printf("The average of all the numbers in the array is : %f",avg);
+}
+int main(){
+    int n;
+    printf("Enter the number of ELements in your array : ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter the elements: \n");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    arrayStats(arr, n);
+    return 0;
+}
