e702488ff08cb1c3bb2ffe7e7f55c0b3c9d4b3c2
Prabeen Kumar Swain <b125082@iiit-bh.ac.in>
2026-07-31 20:06:07 +0530

    Implement dynamic memory allocation for an array
    This program dynamically allocates memory for an array based on user input, takes elements from the user, and prints them.

---
+++ LAB 1/Q8.c
@@ -0,0 +1,31 @@
+//8. Dynamic MemoryAllocation
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter elements:\n");
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nArray Elements:\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    free(arr);
+
+    return 0;
+}
