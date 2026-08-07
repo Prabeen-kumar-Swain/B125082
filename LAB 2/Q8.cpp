62fc00c4b69ed25fea4d6e24781e84338048098b
Prabeen Kumar Swain <b125082@iiit-bh.ac.in>
2026-08-07 22:04:03 +0530

    Create Q8.cpp

---
+++ LAB 2/Q8.cpp
@@ -0,0 +1,43 @@
+//8. Hostel Fee Management
+#include <iostream>
+using namespace std;
+
+class MovieTicket {
+private:
+    string movieName;
+    double ticketPrice;
+    int numberOfTickets;
+
+public:
+    void acceptBookingDetails() {
+        cout << "Enter the name of the movie: ";
+        cin >> movieName;
+    
+        cout << "Enter the price of a single ticket: $";
+        cin >> ticketPrice;
+
+        cout << "Enter the number of tickets purchased: ";
+        cin >> numberOfTickets;
+    }
+
+    double calculateTotalTicketCost() {
+        return ticketPrice * numberOfTickets;
+    }
+
+    void displayBookingSummary() {
+        cout << "\n--- Movie Ticket Details ---\n";
+        cout << "Movie: " << movieName << endl;
+        cout << "Ticket Price: " << ticketPrice << endl;
+        cout << "Number of Tickets: " << numberOfTickets << endl;
+        cout << "Total Cost: "<< calculateTotalTicketCost() <<endl;
+    }
+};
+
+int main() {
+    MovieTicket ticket;
+
+    ticket.acceptBookingDetails();
+    ticket.displayBookingSummary();
+
+    return 0;
+}
